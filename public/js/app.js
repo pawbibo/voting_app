2536a35f43402ef1eb635ac5b5c2d93a61b323f9
pawbibo <jesus.cabelin@bibo.com.ph>
2017-08-11 07:46:50 +0800

    refactor using babel initializer

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,14 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      products: [],
-    };
-  }
+  state = {
+    products: []
+  };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -52,11 +49,9 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   constructor(props) {
     super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
   }
 
-  handleUpVote() {
+  handleUpVote = () => {
     this.props.onVote(this.props.id);
   }
 
